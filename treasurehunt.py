0481bde27c87e680a1a0e7c90e33dede0d0dc5a3
kooidude <skooiman23@bfacademy.de>
2022-03-07 17:08:13 +0100

    Added Name For Testing

--- treasurehunt.py
+++ treasurehunt.py
@@ -10,7 +10,7 @@ print("I am waiting to pass along instructions to one who is worthy.\nWhat is yo
 
 name = input(":")
 
-approvedNamesList = ["Jason", "Andrew"]
+approvedNamesList = ["Jason", "Andrew", "Tom", "Flintlock"]
 approved = False
 
 for testName in approvedNamesList:
