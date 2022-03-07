731e5ad5bccb42d3bd0aeb1f076e590c274bb550
JasonRadcliffe <jason.radcliffe@bfacademy.de>
2022-03-07 17:00:11 +0100

    Adding the name Tom
    Adding the name Tom to the approved list

--- treasurehunt.py
+++ treasurehunt.py
@@ -10,7 +10,7 @@ print("I am waiting to pass along instructions to one who is worthy.\nWhat is yo
 
 name = input(":")
 
-approvedNamesList = ["Jason", "Andrew"]
+approvedNamesList = ["Jason", "Andrew", "Tom"]
 approved = False
 
 for testName in approvedNamesList:
