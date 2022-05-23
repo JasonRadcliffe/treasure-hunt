b3d30b648500406417e6615c38bbea77b33ab5f0
kooidude <skooiman23@bfacademy.de>
2022-05-23 17:29:40 +0200

    Rock Paper Scissors

--- treasurehunt.py
+++ treasurehunt.py
@@ -8,10 +8,15 @@ print ("The room you are in has thick red carpet, and 4 doors, one on each wall"
 print ("You are approached by an elderly man in a suit who asks:\n\n")
 print("I am waiting to pass along instructions to one who is worthy.\nWhat is your first name, explorer?")
 
+import random
+import time
+random.seed(time.time())
+
 name = input(":")
 Position = [ 2 , 2]
 cwtw = 0
 d = 0
+tie = 1
 
 
 room = [[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],
@@ -42,13 +47,18 @@ before your very eyes. The lava rises, and you roast alive."""
 room[2][2][0] = "You find yourself back in the laviously decorated room you began in. The same doors are available to you: North, South, East, and West" 
 room[2][3][0] = ""
 room[2][4][0] = ""
-room[3][0][0] = ""
+room[3][0][0] = """You come upon a gigantic figure sitting in the center of the path, hands upon his knees in a pose as if he were waiting for you. 
+Joints creak as he turns his head to look at you, caked mud and grime falling off him in clumps. 
+His voice sounds as if mountains could speak, \"Hello young traveller. I suppose you are here to get the key I keep over yonder \" He waves off to the East.
+\"However, due to protocol and the such, I cannot simply allow you to have it. Let's play a game of roshambo to see if you may escape, shall we? I haven't seen another living soul in ages.\" 
+The game is simple. When prompted, provide an answer of rock, paper, or scissors. Paper beats rock, rock beats scissors, and scissors beats paper
+\"If you win, you may recieve the key. If you lose, you do not.\""""
 room[3][1][0] = ""
 room[3][2][0] = """It's a hallway, but even so it is yet another magnificent feature of wherever you are. The carpet is luscious and thick, the chandeliers huge and well-lit, and magnificent paintings line the walls. You casually wonder who or what lived in this place, and where they went.
 There are another four doors in here, but the Eastern door seems to lead to stairs, while the Northern has scratches of dirt and grass on the carpet in front of it."""
 room[3][3][0] = ""
 room[3][4][0] = ""
-room[4][0][0] = ""
+room[4][0][0] = "You walk to the shed "
 room[4][1][0] = """Walking through the doorway, you are startled to find yourself atop a high tower, looking down at a series of courtyards below.
 To the North-East you can see a giant figure sitting in one of them seemingly guarding a small storage shed directly to your North, likely used for housing keys.
 There is no other exit off of the tower other than the way you came."""
@@ -175,8 +185,8 @@ def Walk_West():
 	Position[0] -= 1
 
 def Battle():
-	doop = 0
-	play = input()
+	outcome = 0
+	play = input("Enter your selection:")
 	play = play.lower()
 	if play == "rock":
 		best = 1
@@ -185,11 +195,32 @@ def Battle():
 	elif play == "paper":
 		best = 3
 
-	other = randint(1,3)
-	while doop == 1:
-		if best == 1 and other == 2
-			win = 1
-		elif best == 1 and other ==
+	other = random.randint(1,3)
+	if best == 1 and other == 2:
+		outcome = 1
+	elif best == 1 and other == 1:
+		outcome = 2
+	elif best == 1 and other == 3:
+		outcome = 0
+	elif best == 2 and other == 2:
+		outcome = 2
+	elif best == 2 and other == 1:
+		outcome = 0
+	elif best == 2 and other == 3:
+		outcome = 1
+	elif best == 3 and other == 1:
+		outcome = 1
+	elif best == 3 and other == 2:
+		outcome = 0
+	elif best == 3 and other == 3:
+		outcome = 2
+	return outcome
+
+
+
+
+
+
 
 
 def death():
@@ -263,6 +294,9 @@ while stillPlaying == True:
 		Position[1] = int(y)
 	elif nextCommand == "repeat":
 		d += 1
+	elif nextCommand == "battle":
+		bob = Battle()
+		print(bob)
 	else:
 		print("Sorry, please repeat")
 		cwtw = 1
@@ -273,8 +307,34 @@ while stillPlaying == True:
 		if Position[0] == 1 and Position[1] == 1:
 			print(room[Position[0]][Position[1]][0])
 			death()
+		if Position[0] == 3 and Position[1] == 0:
+			if tie == 1:
+				print(room[Position[0]][Position[1]][0])
+			else:
+				print("The giant looks fondly down upon you and wishes you good luck on your journey")
+			while tie == 1:
+				match1 = Battle()
+				if match1 == 1:
+					print("The giant looks surprised, but not too hurt. He smiles and allows you to move along your way to the small shed to his East")
+					tie = 0
+					Position[0] = 4
+					Position[1] = 0
+				if match1 == 0:
+					print("The giant looks rather sad, but not surprised. He gives you the grandest of condolences, and then crushes you under his fist.")
+					tie = 0
+					death()
+				if match1 == 2:
+					print("You tie, and the giant asks for another go.")
+					tie = 1
+		if Position[0] == 4 and Position[1] == 0:
+			print(room[4][0][0])
+			key1 = 1
+
 		else:
 			print(room[Position[0]][Position[1]][0])
+			print("bob")
+
+
 	cwtw = 0
 
 
